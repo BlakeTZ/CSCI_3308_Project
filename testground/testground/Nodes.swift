cc90e11588bd67a999bbcc3705a1700eed22151a
Zhuangzhuang Jiang <zhji3185@colorado.edu>
2017-04-25 06:04:33 +0800

    Add files via upload

--- testground/testground/Nodes.swift
+++ testground/testground/Nodes.swift
@@ -1,15 +1,1 @@
-﻿//
-//  Nodes.swift
-//  testground
- 
-
-import SpriteKit
-
-class Projectile: SKShapeNode {
-    convenience init(path: UIBezierPath, color: UIColor, borderColor:UIColor) {
-        self.init()
-        self.path = path.cgPath
-        self.fillColor = color
-        self.strokeColor = borderColor
-    }
-}
+﻿
